9223ff0a0a045d640fe94b2ccc1553589fe5c727
Aakash Soni <inos92@gmail.com>
2023-04-15 22:08:03 -0500

    added function to combine strings into single string w/ comma separator

--- practice.js
+++ practice.js
@@ -122,3 +122,17 @@
 //   return results.length;
 // }
 // console.log(beginswitha(["aakash", "apple", "ball", "afar"]));
+
+//Write a function that takes in an array of strings and joins them together to make a single string separated by commas.
+function singlestring(strings) {
+  let result = [];
+  for (let i = 0; i < strings.length - 1; i++) {
+    result = result + strings[i] + ",";
+  }
+  result = result + strings.slice("-1");
+  // strings.forEach(function (string) {     <<< initially used this code but wanted to refine it to remove comma after last string
+  //   result = result + string + ", ";
+  // });
+  return result;
+}
+console.log(singlestring(["hello", "how", "are", "you", "today"]));

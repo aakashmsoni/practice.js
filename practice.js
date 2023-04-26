c185611a00a2a66008754dad8d23300bb4825549
Aakash Soni <inos92@gmail.com>
2023-04-26 12:45:33 -0500

    added practice

--- practice.js
+++ practice.js
@@ -146,3 +146,17 @@
 //   return result;
 // }
 // console.log(computeProduct([1, 2, 3, 4]));
+
+// You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+// function birthdayCakeCandles(candles) {
+//   candles.sort(function (a, b) {
+//     return b - a;
+//   });
+//   let tallestCandles = [candles[0]];
+//   for (let i = 1; i < candles.length; i++) {
+//     if (candles[i] >= candles[0]) {
+//       tallestCandles.push(candles[i]);
+//     }
+//   }
+//   return tallestCandles.length;
+// }
